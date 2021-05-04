9d3da8e5822c44b1d3db664f5867298ee0f94d8e
shiqstone <shiqstone@gmail.com>
2021-05-04 14:01:32 +0800

    optimize

--- code37/sudoku_solution.go
+++ code37/sudoku_solution.go
@@ -1,14 +1,21 @@
 package code37
 
-func solve(board [][]byte) [][]byte {
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
+func solveSudoku(board [][]byte) [][]byte {
+	return solve(board, 0, 0)
+}
+
+func solve(board [][]byte, i, j int) [][]byte {
+	for ; i < 9; i++ {
+		if j >= 9 {
+			j = 0
+		}
+		for ; j < 9; j++ {
 			if board[i][j] == '.' {
 				var char byte = '1'
 				for char <= '9' {
 					if isValid(board, i, j, char) {
 						board[i][j] = char
-						res := solve(board)
+						res := solve(board, i, j+1)
 						if res != nil {
 							return res
 						} else {
--- code37/sudoku_solution_test.go
+++ code37/sudoku_solution_test.go
@@ -44,7 +44,7 @@ func Test_solve(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := solve(tt.args.board); !reflect.DeepEqual(got, tt.want) {
+			if got := solveSudoku(tt.args.board); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("solve() = %v, want %v", got, tt.want)
 			}
 		})
