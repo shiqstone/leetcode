330f67e26b4651957e8e6130fc0e097f7f687b6d
shiqstone <shiqstone@gmail.com>
2021-05-06 13:48:57 +0800

    update

--- code41/first_missing_positive.go
+++ code41/first_missing_positive.go
@@ -4,21 +4,14 @@ func firstMissingPositive(nums []int) int {
 	slen := len(nums)
 	for i := 0; i < slen; i++ {
 		for nums[i] > 0 && nums[i] <= slen && nums[i] != nums[nums[i]-1] {
-			swap(nums, i, nums[i]-1)
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 		}
 	}
 
-	for k, v := range nums {
-		if v != k+1 {
-			return k + 1
+	for i := 0; i < slen; i++ {
+		if nums[i] != i+1 {
+			return i + 1
 		}
 	}
 	return slen + 1
-}
-
-func swap(nums []int, i, j int) []int {
-	tmp := nums[i]
-	nums[i] = nums[j]
-	nums[j] = tmp
-	return nums
 }
